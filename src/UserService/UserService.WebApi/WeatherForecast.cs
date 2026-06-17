8e044266c93a83eef0608fd62bc7042179e030c9
Olszewskioc <thiago.olszewskioc@gmail.com>
2026-06-16 22:39:17 -0400

    Test

--- src/UserService/UserService.WebApi/WeatherForecast.cs
+++ src/UserService/UserService.WebApi/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace UserService.WebApi
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.55566);
 
         public string? Summary { get; set; }
     }
