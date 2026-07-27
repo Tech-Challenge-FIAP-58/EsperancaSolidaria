e06e363b96929c52acf81d1ad719da7533526e8d
David <2dsant3@gmail.com>
2026-07-27 00:53:36 -0300

    teste p rodar ci

--- src/UserService/UserService.Domain/Models/User.cs
+++ src/UserService/UserService.Domain/Models/User.cs
@@ -1,7 +1,7 @@
 namespace UserService.Domain.Models
 {
     public class User : EntityBase
-    {
+    { 
         public required string Name { get; set; } = string.Empty;
         public required string Email { get; set; } = string.Empty;
         public required string Password { get; set; } = string.Empty;
